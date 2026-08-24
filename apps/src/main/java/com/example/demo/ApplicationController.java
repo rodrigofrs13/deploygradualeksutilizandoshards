6c5e392c1a875a1def9efb79f5d9532ae48bf6f6
Rodrigo Fernandes <rodrigofrs13@gmail.com>
2026-08-24 19:33:22 -0300

    Nova versão - Teste 14

--- apps/src/main/java/com/example/demo/ApplicationController.java
+++ apps/src/main/java/com/example/demo/ApplicationController.java
@@ -19,7 +19,7 @@ public class ApplicationController {
     public Map<String, String> home() {
         return Map.of(
             "application", "springboot-sharded-app",
-            "message", "Teste 13",
+            "message", "Teste 14",
             "version", version,
             "shard", shard
         );
--- terraform/platform/argoworkflows-poller.tf
+++ terraform/platform/argoworkflows-poller.tf
@@ -111,7 +111,7 @@ resource "kubectl_manifest" "argo_workflow_poller_cronworkflow" {
       # CronWorkflow com "spec.schedules: Required value" se só "schedule"
       # for enviado).
       schedules:
-        - "*/1 * * * *"
+        - "*/2 * * * *"
       timezone: "UTC"
       concurrencyPolicy: "Forbid"
       startingDeadlineSeconds: 30
