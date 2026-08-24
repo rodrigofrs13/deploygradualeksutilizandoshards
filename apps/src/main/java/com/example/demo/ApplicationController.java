dff965b8c3ce4b1d3008bf6d448ea29e99ff9ca8
Rodrigo Fernandes <rodrigofrs13@gmail.com>
2026-08-24 18:44:55 -0300

    Nova versão - Teste 9

--- apps/src/main/java/com/example/demo/ApplicationController.java
+++ apps/src/main/java/com/example/demo/ApplicationController.java
@@ -19,7 +19,7 @@ public class ApplicationController {
     public Map<String, String> home() {
         return Map.of(
             "application", "springboot-sharded-app",
-            "message", "Teste 8",
+            "message", "Teste 9",
             "version", version,
             "shard", shard
         );
