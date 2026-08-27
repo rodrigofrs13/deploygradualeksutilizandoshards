929574743be4ba84c227efa8e980fc881a0c13fe
Rodrigo Fernandes <rodrigofrs13@gmail.com>
2026-08-27 18:11:37 -0300

    Teste 3 ec2

--- scripts/01-get-hostname-canary.sh
+++ scripts/01-get-hostname-canary.sh
@@ -1,3 +1,14 @@
+echo ""
+echo "###########  Shard1"
+
 kubectl get svc springboot-canary -n shard-1
 export SHARD1_CANARY_LB=$(kubectl get svc springboot-canary -n shard-1 -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
-curl http://$SHARD1_CANARY_LB/
+curl -s http://$SHARD1_CANARY_LB/ | jq-windows-amd64.exe .
+
+
+echo ""
+echo "###########  Shard2"
+
+kubectl get svc springboot-canary -n shard-2
+export SHARD2_CANARY_LB=$(kubectl get svc springboot-canary -n shard-2 -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+curl -s http://$SHARD2_CANARY_LB/ | jq-windows-amd64.exe .
