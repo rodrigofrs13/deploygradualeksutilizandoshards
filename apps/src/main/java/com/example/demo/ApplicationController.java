3eca50c59f945e638172b997d9a6e7bfee9c22a0
Rodrigo Fernandes <rodrigofrs13@gmail.com>
2026-08-27 19:56:24 -0300

    Teste 29

--- apps/src/main/java/com/example/demo/ApplicationController.java
+++ apps/src/main/java/com/example/demo/ApplicationController.java
@@ -19,7 +19,7 @@ public class ApplicationController {
     public Map<String, String> home() {
         return Map.of(
             "application", "springboot-sharded-app",
-            "message", "Teste 27",
+            "message", "Teste 29",
             "version", version,
             "shard", shard
         );
